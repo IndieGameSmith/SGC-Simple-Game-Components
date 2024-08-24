50ede22f5d8d6f18f0306cbb113667c412f65072
IndieGameSmith <165539000+IndieGameSmith@users.noreply.github.com>
2024-08-24 22:31:02 +0530

    Exceptions.hpp

---
+++ include/Utils/Exceptions.hpp
@@ -0,0 +1,63 @@
+/** 
+ *  \file Exceptions.hpp
+ *  
+ *  \brief Header for Various Exceptions.
+ *
+ *  This supports various Exceptions.
+ *
+ *  \author IndieGameSmith
+ *  \date 2024-08-23
+ */
+
+#ifndef EXCEPTIONS_HPP_
+#define EXCEPTIONS_HPP_
+
+#include <string>
+#include <exception>
+
+namespace Exception
+{
+
+class DivisionByZero : public std::exception
+{
+
+public:
+    DivisionByZero(const char* p_message) : message(p_message) {};
+    
+    const char* what() const noexcept override 
+    {
+        return message;
+    }
+    
+    ~DivisionByZero()
+    {
+        delete message;
+    }
+    
+private:
+    const char* message;
+};
+
+class InvalidRoundFlag : public std::exception
+{
+
+public:
+    InvalidRoundFlag(const char* p_message) : message(p_message) {};
+    
+    const char* what() const noexcept override
+    {
+        return message;
+    }
+    
+    ~InvalidRoundFlag()
+    {
+        delete message;
+    }
+    
+private:
+    const char* message;
+};
+
+}
+
+#endif // EXCEPTIONS_HPP_
