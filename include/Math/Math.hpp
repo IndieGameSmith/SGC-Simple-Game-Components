5afa15fb31dd25425bc63b8ebb86f90d7c25fe27
IndieGameSmith <165539000+IndieGameSmith@users.noreply.github.com>
2024-10-08 10:32:10 +0530

    Update Math.hpp

--- include/Math/Math.hpp
+++ include/Math/Math.hpp
@@ -1,1 +1,22 @@
+/** 
+ *  \file Math_Typedef.hpp
+ *  
+ *  \brief Header file that contians all Mathematical Functions and classes.
+ *
+ *  \author IndieGameSmith
+ *  \date 2024-08-26
+ */
+ 
+#ifndef MATH_HPP_
+#define MATH_HPP_
 
+#include "Vector2D.hpp"
+#include "Vector3D.hpp"
+#include "Point2D.hpp"
+#include "Point3D.hpp"
+#include "Matrix2D.hpp"
+#include "Matrix3D.hpp"
+#include "Math_Typedefs.hpp"
+#include "Math_Utils.hpp"
+
+#endif // MATH_HPP_
