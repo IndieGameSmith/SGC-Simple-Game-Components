ca21b26bc56fd7f1f445c8b8da92d6b2a159f56e
IndieGameSmith <165539000+IndieGameSmith@users.noreply.github.com>
2024-10-08 12:25:57 +0530

    Update Window.hpp

--- include/Graphics/Window.hpp
+++ include/Graphics/Window.hpp
@@ -10,15 +10,35 @@
 #ifndef WINDOW_HPP_
 #define WINDOW_HPP_
 
+#include "SDL2/SDL.h"
+#include "Math.hpp"
+
+namespace Game
+{
+namespace Graphics
+{
+
 class Window
 {
 public:
-    Window(const char* p_title, const int p_w, const int p_h);
-    Create(const char* p_title, const int p_w, const int p_h);
+    Window(const char* p_title, int p_w, int p_h)a;
+    void Create(const char* p_title, int p_w, int p_h);
+    void ResizeWindow(int w, int h);
+    void MoveWindowTo(int x, int y);
+    void CenterWindow();
+    void ToggleWindowVisiblitt();
+    void ToggleFullscreen();
+    void ToggleBorder();
+    void Destroy();
     
-    Resize
+    Math::Pair GetWindowCenter();
 private:
     SDL_Window* window;
-}
+    Math::Point2D windowPos;
+};
+
+} // namepace Graphics
+
+} // namespace Game
 
 #endif // WINDOW_HPP_
