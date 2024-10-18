2758b1727510e431b4167271720cfeddd945213e
AreYouTimmy <khusboosingh1488@gmail.com>
2024-10-18 20:08:02 +0530

    Update Texture.hpp

--- include/Entity/Texture.hpp
+++ include/Entity/Texture.hpp
@@ -32,6 +32,8 @@ public:
 	int GetHeight() const;
 	SDL_Texture* GetSDLtexture() const;
 	
+	void Render(SDL_Renderer* renderer, Math::Vector2D<int> pos, double scale = 1, double angle = 0.00f, SDL_Point* center = nullptr, SDL_RendererFlip flip = SDL_FLIP_NONE);
+	
 private:
 	SDL_Texture* texture;
 	int width, height;
